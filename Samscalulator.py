a5a1e7768c6d2534a91b3914e16b0279b1bed699
samdoolan <47331717+samdoolan@users.noreply.github.com>
2019-04-15 17:17:28 -0700

    Add code file

---
+++ Samscalulator.py
@@ -0,0 +1,28 @@
+
+print("Please type the  operation of your choice:")
+print("+ for addition")
+print("- for subtraction")
+print("* for multiplication")
+print("/ for division")
+
+
+number_1 = float(input("Enter number: "))
+operator = input("Enter operation: ")
+number_2 = float(input("Enter number: "))
+
+
+if operator == "+":
+    print(number_1 + number_2)
+elif operator == "-":
+    print(number_1 - number_2)
+elif operator == "/" :
+    print(number_1 / number_2)
+elif operator == "*":
+    print(number_1 * number_2)
+elif operator == "#sex":
+    print("yes daddy")
+elif number_1 == "69":
+    print("u horny fuck")
+else:
+    print("error 69420: you have fucked up")
+
